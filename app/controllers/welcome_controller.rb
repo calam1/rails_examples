7d092e294e9614fd476d11195549e1288ae22f7f
Chris Lam <>
2019-07-05 14:47:25 -0500

    basic devise https://medium.com/@mchisti/creating-simple-users-in-rails-with-devise-gem-tutorial-cd91d2ef36d5 and http://www.korenlc.com/rails-tutorial-authentication-with-devise/ as references

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,12 @@
 class WelcomeController < ApplicationController
+  # before any blog action happens, it will authenticate the user  before_action :authenticate_user!
+
   def index
+    #handle this in the view, welcome/index.html.erb
+#    if current_user
+#      @user = current_user.email
+#    else
+#      redirect_to new_user_session_path
+#    end
   end
 end
