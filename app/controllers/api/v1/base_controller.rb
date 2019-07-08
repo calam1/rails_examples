f15bf95bc70d5bcd3671e79823b0e20433100c87
Chris Lam <>
2019-07-08 16:21:02 -0500

    https://medium.com/@goncalvesjoao/rails-devise-jwt-and-the-forgotten-warden-67cfcf8a0b73 as reference to add jwt token as a strategy

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -3,6 +3,7 @@ class Api::V1::BaseController < ActionController::Base
   protect_from_forgery with: :null_session
 
   before_action :destroy_session
+  before_filter :authenticate_user!
 
   def destroy_session 
     request.session_options[:skip] = true 
